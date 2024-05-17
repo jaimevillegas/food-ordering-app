b2ce7c5c6d4dd5b6b2b5e5facf31bf15ba467aa0
Jaime Villegas <jaimevillegas296@gmail.com>
2024-05-17 08:30:11 -0500

    Add Hero and Header components

--- src/app/page.js
+++ src/app/page.js
@@ -1,25 +1,11 @@
-import Link from "next/link";
+import Header from "@/components/layout/Header";
+import Hero from "@/components/layout/Hero";
 
 export default function Home() {
   return (
     <>
-      <header className="flex items-center justify-between">
-        <Link className="text-primary font-semibold text-2xl" href="#">
-          ST PIZZA
-        </Link>
-        <nav className="flex items-center gap-8 text-gray-500 font-semibold">
-          <Link href={""}>Home</Link>
-          <Link href={""}>Menu</Link>
-          <Link href={""}>About</Link>
-          <Link href={""}>Contact</Link>
-          <Link
-            className="bg-primary text-white px-8 py-2 rounded-full"
-            href={""}
-          >
-            Login
-          </Link>
-        </nav>
-      </header>
+      <Header />
+      <Hero />
     </>
   );
 }
